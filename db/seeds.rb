49b2cc329c7a722802740438e843fe4a4f978498
King <kelkab46@gmail.com>
2023-03-28 22:35:32 +0300

    added migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Lets go"
+hero = Hero.create([  
+
+    {  "name": "Kamala Khan", "super_name": "Ms. Marvel" },  
+    
+    {  "name": "Doreen Green", "super_name": "Squirrel Girl" },  
+    
+    {  "name": "Gwen Stacy", "super_name": "Spider-Gwen" }
+    
+    ])
+
+power = Power.create([
+    {
+      "name": "super strength",
+      "description": "gives the wielder super-human strengths"
+    },
+    {
+      "name": "flight",
+      "description": "gives the wielder the ability to fly through the skies at supersonic speed"
+    }
+  ])    
+ 
+hero-power = HeroPower.create([{
+    "strength": "Strong",
+    "power_id": 2,
+    "hero_id": 3
+  },
+  {
+    "strength": "Average",
+    "power_id": 1,
+    "hero_id": 3
+  },
+  {
+    "strength": "Average",
+    "power_id": 1,
+    "hero_id": 2
+  }])  
+
+  puts "We done"
